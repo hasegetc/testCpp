c61573c15dd85cf446487e6de766a3b98eb03494
liuyuchao <hubeiliuyuchao@gmail.com>
2020-07-29 16:05:59 +0800

    add test

--- testCpp.cpp
+++ testCpp.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 int main() {
     cout << "Hello World!" << endl;
+
+    int result = 0;
+    for(int i = 1; i <= 100; i++) {
+        result += i;
+    }
+    cout << "liuyuchao";
+    cout << result << endl;
+
     return 0;
 }
